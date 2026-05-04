7706d558cf38c79f90de2399ed27c61bfeb1a944
vanshupreti04 <vanshupreti0412@gmail.com>
2026-05-04 18:43:46 +0530

    Added the complete working backend with dockerization

---
+++ src/main/java/com/email/writer/app/CorsConfig.java
@@ -0,0 +1,28 @@
+package com.email.writer.app;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.*;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+
+                registry.addMapping("/api/**")
+                        .allowedOrigins(
+                                "https://mail.google.com",
+                                "https://replyai.cfd",
+                                "https://email-writer-frontend-lyart.vercel.app"
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
